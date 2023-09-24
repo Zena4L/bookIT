8c778f9a51591be2d7d1b37e45f45c3b8bf0fe3b
Clement Owireku-Bogyah <zenerbogyah7@gmail.com>
2023-09-24 21:54:40 +0000

    minor changes

--- auth/README.md
+++ auth/README.md
@@ -1,6 +1,6 @@
 # Authentication Service
 
-- POST /api/users/signup {email:string, password:string}
+- POST /api/users/signup
 - POST /api/users/signin
 - POST /api/users/signout
 - GET /api/users/currentuser
--- auth/src/errors/customError.ts
+++ auth/src/errors/customError.ts
@@ -1,6 +1,8 @@
 /**
  * This is an abstract class of how operational errors should be
  * It must have an output which returns an array of errors
+ *  @ statusCode - this indicate the status code
+ *  @ output - this is the serialized error message
  */
 
 export abstract class CustomError extends Error {
