a68e9cea3bac96bb22e14393c1aeefcc7078819b
Clement Owireku-Bogyah <zenerbogyah7@gmail.com>
2023-10-02 15:36:01 +0000

    mongo_uri config

--- auth/src/server.ts
+++ auth/src/server.ts
@@ -3,13 +3,13 @@ import { createServer } from "http";
 import mongoose from "mongoose";
 
 const start = async () => {
-  if (!process.env.JWT_KEY) {
+  if (!process.env.JWT_KEY && process.env.MONGO_URI) {
     throw new Error("No jwt key");
     process.exit(1);
   }
 
   try {
-    await mongoose.connect("mongodb://auth-mongo-srv:27017/auth");
+    await mongoose.connect(process.env.MONGO_URI!);
     console.log("Database Connected");
   } catch (err) {
     console.log(err);
