de1da29ff0b6b048260cfd9e7c3b947050fc9ed8
Clement Owireku-Bogyah <zenerbogyah7@gmail.com>
2023-10-06 07:59:16 +0000

    updates

--- resources/src/middlewares/notAuthorized.ts
+++ resources/src/middlewares/notAuthorized.ts
@@ -1,7 +1,7 @@
 import { RequestHandler } from "express";
 import { RequireAuth } from "../errors/requireAuth";
 
-export const requireAuth: RequestHandler = (req, res, next) => {
+export const notAuthorized: RequestHandler = (req, res, next) => {
   if (!req.currentUser) {
     return next(new RequireAuth());
   }
